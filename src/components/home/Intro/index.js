1576822a48e62161a37bd7f2953dbc4b16a8fbe6
BOKILPATRICK <bkilpatrick@judge.com>
2018-12-27 23:52:06 -0500

    Integrated IG feed. Striped down intro section.

--- src/components/home/Intro/index.js
+++ src/components/home/Intro/index.js
@@ -1,58 +1,12 @@
 import React from 'react'
+import './intro.scss';
 
 const Intro = () => (
-  <div className="wrapper light-wrapper">
-    <div className="container inner">
-      <div className="row gutter-60 gutter-md-30">
-        <div className="col-md-6">
-          <h2 className="sub-heading text-center text-md-right">
-            I'm a photographer based in New York. I take photographs with
-            creativity, concept and passion.
-          </h2>
-        </div>
-        <div className="col-md-6">
-          <p className="lead text-center text-md-left">
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam
-            venenatis vestibulum. Morbi leo risus, porta ac consectetur ac,
-            vestibulum at eros.
-          </p>
-        </div>
-      </div>
-      <div className="space40" />
-      <div className="row gutter-50 process-wrapper line text-center">
-        <div className="col-md-3">
-          <span className="icon icon-bg bg-default mb-20">
-            <span className="number">01</span>
-          </span>
-          <h5>Concept</h5>
-          <p>Nulla vitae elit libero elit non porta gravida eget metus cras.</p>
-        </div>
-        <div className="col-md-3">
-          <span className="icon icon-bg bg-default mb-20">
-            <span className="number">02</span>
-          </span>
-          <h5>Prepare</h5>
-          <p>Nulla vitae elit libero elit non porta gravida eget metus cras.</p>
-        </div>
-        <div className="space20 d-md-none clearfix" />
-        <div className="col-md-3">
-          <span className="icon icon-bg bg-default mb-20">
-            <span className="number">03</span>
-          </span>
-          <h5>Retouch</h5>
-          <p>Nulla vitae elit libero elit non porta gravida eget metus cras.</p>
-        </div>
-        <div className="col-md-3">
-          <span className="icon icon-bg bg-default mb-20">
-            <span className="number">04</span>
-          </span>
-          <h5>Finalize</h5>
-          <p>Nulla vitae elit libero elit non porta gravida eget metus cras.</p>
-        </div>
-      </div>
+  <div className="intro-wrapper light-wrapper">
+    <div className="text-content">
+      <h1>Our passion is to capture your once in a lifetime moment or vision. Creatively. Professionally.</h1>
     </div>
   </div>
-)
+);
 
 export default Intro
--- src/components/shared/Instafeed/index.js
+++ src/components/shared/Instafeed/index.js
@@ -14,26 +14,26 @@ class InstagramFeed extends Component {
         <div className="container inner">
           <h2 className="section-title text-center">Instagram Feed</h2>
           <p className="lead text-center">
-            Here you will find the latest shots from our Instagram feed.
+            Check out our latest shots
           </p>
           <div className="space20" />
           <div className="tiles tiles-s">
             <div id="instafeed" className="items row">
               <Instafeed
-                userId="1215763826"
-                limit="6"
-                ref="instafeed"
-                resolution="low_resolution"
-                sortBy="most-recent"
-                target="instafeed"
-                template={`<div class="item col-6 col-sm-4 col-md-2"><figure className="overlay overlay3"><a href="{{link}}" target="_blank"><span className="bg"></span><img src="{{image}}" /><figcaption className="d-flex"><div className="align-self-center mx-auto"><i className="fa fa-instagram"></i></div></figcaption></figure></div>`}
-                accessToken="1215763826.f1627ea.512d3a9b334a4c91ac2e83d4f4d9b291"
+                  userId="9144106797"
+                  limit="6"
+                  ref="instafeed"
+                  resolution="low_resolution"
+                  sortBy="most-recent"
+                  target="instafeed"
+                  template={`<div class="item col-6 col-sm-4 col-md-2"><figure className="overlay overlay3"><a href="{{link}}" target="_blank"><span className="bg"></span><img src="{{image}}" /><figcaption className="d-flex"><div className="align-self-center mx-auto"><i className="fa fa-instagram"></i></div></figcaption></figure></div>`}
+                  accessToken="9144106797.1677ed0.7472ecf190294ac29b14d7d3ea7e448e"
               />
             </div>
           </div>
           <div className="space40" />
           <div className="text-center">
-            <a href="#" className="btn btn-full-rounded">
+            <a href="https://www.instagram.com/23twentymedia/" className="btn btn-full-rounded" target="_blank">
               Instagram Page
             </a>
           </div>
