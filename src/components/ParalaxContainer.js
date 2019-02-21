2090ccb40a95df966e974d23c1a9c7ad26f3b20e
Brian Kilpatrick <brian@23twenty.com>
2019-02-20 20:16:15 -0500

    Fix for parallax on mobile

--- src/components/ParalaxContainer.js
+++ src/components/ParalaxContainer.js
@@ -1,11 +1,29 @@
 import React from 'react';
 
-const ParallaxContainer = ({imgUrl, caption, className}) => (
-    <div className={`wrapper image-wrapper bg-image inverse-text ${className}`} style={{backgroundImage: `url(${imgUrl})`}}>
-      <div className="container inner pt-120 pb-120 text-center">
-        <h1 className="heading mb-0">{caption}</h1>
-      </div>
-    </div>
-);
+class ParallaxContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state= {
+           isMobile: false
+        }
+    }
+
+    componentDidMount() {
+        this.setState({isMobile: window.innerWidth < 768})
+    }
+
+    render() {
+        const { imgUrl, caption, className } = this.props;
+        const { isMobile } = this.state;
+        return (
+            <div className={`wrapper image-wrapper bg-image inverse-text ${className} ${isMobile ? 'mobile' : ''}`} style={{backgroundImage: `url(${imgUrl})`}}>
+                <div className="container inner pt-120 pb-120 text-center">
+                    <h1 className="heading mb-0">{caption}</h1>
+                </div>
+            </div>
+        )
+    }
+}
 
 export default ParallaxContainer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -52,8 +52,7 @@ class Index extends React.Component {
                     <div className="col-md-9">
                         <Img className="my-portrait" fluid={myImg}/>
                         <p className="intro-content">Hey! My name is Brian Kilpatrick. I started 23Twenty Media out of an insatiable
-                            appetite for high quality imagery. Capturing once in a lifetime moments, ideas, & brands, creatively
-                            & beautifully is my passion. I'd love to work with you to create something great!
+                            appetite for high quality imagery. Creatively & beautifully capturing once in a lifetime moments, ideas, & brands is my passion. I'd love to work with you to create something great!
                             I'm also a husband, father, software developer, musician, & die-hard Philadelphia Eagles fan.
                             Shoot me an email and lets talk! <a href="mailto:brian@23twenty.com">brian@23twenty.com</a>
                         </p>
