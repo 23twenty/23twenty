afc7231cce3ce0ee8e20e13fd35c155994012bba
Brian Kilpatrick <brian@23twenty.com>
2019-10-08 17:04:34 -0400

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4f1f954576f3f9f6798a.js"
+    "url": "webpack-runtime-2fac6cb1abfdddf98ca5.js"
   },
   {
     "url": "app-9f915c2890f95d17edab.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3c925dd3922392a5c59c8e9bf02b2f69"
+    "revision": "16e0c8c19c7c733291a64d032cc4e6dc"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json",
